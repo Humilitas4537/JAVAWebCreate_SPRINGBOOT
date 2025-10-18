7a89a4dec7b3e32beb5cd13eb6f85c5d357cca6c
Hmilitas4537 <msms13075@gmail.com>
2025-10-18 20:33:16 +0900

    2주차 자바웹 연습문제 완료 및 복습

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.model.domain.TestDB;
 import com.example.demo.model.service.TestService; // 최상단 서비스 클래스 연동 추가
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@Controller // 컨트롤러 어노테이션 명시
+@Controller // 컨트롤러 어노테이션 = View를 제공하는 컨트롤러로 설정. 
+// 매핑핸들러가 @Controller가 붙은 클래스에서 매핑을 찾아 url 요청을 처리
 public class DemoController {
     // 클래스 하단 작성
     @Autowired
@@ -18,9 +21,22 @@ public class DemoController {
 
     @GetMapping("/hello") // 전송 방식 GET
     public String hello(Model model) {
-        model.addAttribute("data", " 방갑습니다."); // model 설정
-        return "hello"; // hello.html 연결
+        model.addAttribute("data", "방갑습니다."); // model 설정
+        // 모델은 페이지에 그려질 정보로써 View에 데이터를 키 값으로 전달
+        return "hello"; // viewName 반환
+        // 자동으로 ViewResolver가 .html로 연결하여 템플릿 폴더에서 찾음
     }
+
+    @GetMapping("/hello2")
+    public String hello2(Model model) {
+        model.addAttribute("data1", "model로 페이지에 그릴 정보를 전달합니다.");
+        model.addAttribute("data2", "model은 key-value 값으로,");
+        model.addAttribute("data3", "key를 이용해 html에 값을 출력합니다.");
+        model.addAttribute("data4", "사용법은 th:text='${key}'");
+        model.addAttribute("data5", "${}는 변수 표현식으로 {}안에 key를 넣습니다.");
+        return "hello2";
+    }
+    
 
     @GetMapping("/about_detailed")
     public String about() {
